8dd90cd69eca8a1ba0cd97e1b7ce0052ec93a946
thiagopicorelli <thiagopicorellichalet@gmail.com>
2022-08-24 18:13:11 -0300

    Realiza desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,22 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(arr) {
+  if (arr.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  let count = new Array(10).fill(0);
+  for (let num of arr) {
+    count[num] += 1;
+    if (Number.isNaN(count[num]) || count[num] >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  const ddd = `${arr[0]}${arr[1]}`;
+  const num1 = `${arr[2]}${arr[3]}${arr[4]}${arr[5]}${arr[6]}`;
+  const num2 = `${arr[7]}${arr[8]}${arr[9]}${arr[10]}`;
+  return `(${ddd}) ${num1}-${num2}`;
 }
+
+console.log(generatePhoneNumber([0, 21, 3, 4, 14, 2, 7, 8, 19, 9, 4]));
 
 // Desafio 12
 function triangleCheck() {
